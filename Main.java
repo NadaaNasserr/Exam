f64dc48de5ef5c333611b77fd9d8844c62bfc07a
NadaaNasserr <148685934+NadaaNasserr@users.noreply.github.com>
2023-10-31 11:06:38 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Scanner S = new Scanner(System.in);
+
+                       //E1
+//        System.out.println("enter any number");
+//        int num =S.nextInt();
+//
+//        if( num % 2 == 0){
+//            System.out.println("the number is even");
+//        }
+//        else System.out.println("the number is odd");
+
+                //E2
+//        System.out.println("please enter the number Or 0 to stop");
+//
+//        int num = S.nextInt();
+//        int sum =0;
+//
+//        while (num != 0){
+//            System.out.println("please enter the number or 0 to stop");
+//            sum+=num;
+//            num = S.nextInt();
+//        }
+//        System.out.println("The sum of number = " + sum);
+
+
+        //3
+//        int[] array = {10, -21, 30 , 31, -25};
+//
+//        for (int i = 0; i<array.length; i++){
+//
+//            if (array[i] > 0){
+//                System.out.println(array[i] + " is a posititve number");
+//            }
+//            else System.out.println(array[i] + " is a negative number ");
+//        }
+
+
+        //4
+
+//        String[] array = {"Tuwaiq" ,"Bootcamp" , "Student", "JAVA" };
+//        String shortest = "";
+//        int num = 0;
+//
+//        for (int i =0; i < array.length; i++ ){
+//
+//            if(array[i].length() < num )
+//                num = array[i].length();
+//                shortest = array[i];
+//        }
+//        System.out.println(shortest);
+
+        //
+//
+//        String[] array = {"Tuwaiq", "Bootcamp" , "Student", "JAVA" };
+//        String shortest= "";
+//
+//
+//        for (int i =0; i< array.length; i++ ){
+//
+//          int num= array[0].length();
+//            if(array[i].length() < num ){
+//                shortest = array[i];
+//
+//        }
+//        }
+//
+//        System.out.println(shortest);
+
+
+
+
+    }
+}
+
+
